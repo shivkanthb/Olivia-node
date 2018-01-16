7c5860e1cdd4c278fbc9b82e474e7063568f6745
shivkanthb <shivkanthb@gmail.com>
2018-01-15 22:45:35 -0700

    get_candidate call

--- index.js
+++ index.js
@@ -7,24 +7,30 @@ var actions = {
 	get_conversations: { endpoint: '/company/conversations', method: 'GET' },
 	get_locations: { endpoint: '/company/locations', method: 'GET' },
 	create_candidate: { endpoint: '/candidates', method: 'POST' },
-	get_candidates: { endpoint: '/candidates', method: 'GET' }
+	get_candidates: { endpoint: '/candidates', method: 'GET' },
+  get_candidate: { endpoint: '/candidates/{candidate_id}', method: 'GET' }
 }
 
 function make_request(action, data, callback) {
 	let method = actions[action].method;
 	let options;
+  let endpoint = actions[action].endpoint;
 	if (method == 'POST' || method == 'PUT') {
 		options = { 
 	        method: actions[action].method,
-	        url: base_url + actions[action].endpoint,
+          headers: { "authorization" : "token " + api_token},
+	        url: base_url + endpoint,
 	        json: true,
 	        body: data
     	};
 	} else if (method == 'GET') {
+    if (action == "get_candidate") {
+      endpoint = endpoint.replace("{candidate_id}", data['candidate_id']);
+    }
 		options = { 
 	        method: actions[action].method,
 	        headers: { "authorization" : "token " + api_token},
-	        url: base_url + actions[action].endpoint,
+	        url: base_url + endpoint,
 	        qs: data
     	};
 	}
@@ -47,7 +53,19 @@ module.exports = {
     	make_request('get_candidates', qs, function(err, resp, body) {
     		callback(err, body);
     	})
+    },
 
+    get_candidate: function(candidate_id, data, callback) {
+      let candidate_info = {
+        candidate_id: candidate_id
+      }
+      if (!api_token) {
+        return callback(new Error("Token not set"));
+      }
+      let qs = Object.assign(candidate_info, data);
+      make_request('get_candidate', qs, function(err, resp, body) {
+        callback(err, body);
+      })
     },
 
    	getToken: function(keys, callback) {
